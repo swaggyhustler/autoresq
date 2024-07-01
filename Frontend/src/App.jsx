96d19d246379ac814797ef6f8f688c21313f7fdb
swaggyhustler <gedamsubhash086@gmail.com>
2024-07-01 19:57:16 +0530

    Add initial files for Frontend (react)

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,126 @@
+
+const App = () => {
+  return (
+    <nav className="ajb" data-headlessui-state="">
+      <div className="gx uh aro cgf det">
+        <div className="lx nr zm">
+          <div className="lx">
+            <div className="hs km lx zg cmv">
+              <button
+                className="ab ly zg zl aeb aqw axv bim blx bnc bnh bni boe"
+                id="headlessui-disclosure-button-:r0:"
+                type="button"
+                aria-expanded="false"
+                data-headlessui-state=""
+              >
+                <span className="aa ae"></span>
+                <span className="t">Open main menu</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                  className="lu oi sl"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  ></path>
+                </svg>
+              </button>
+            </div>
+            <div className="lx uw zg">
+              <img
+                className="om tt"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                alt="Your Company"
+              />
+            </div>
+            <div className="md cmc cmr cok cpb">
+              <a href="#" className="aje ban aeb arl asb awg awk" aria-current="page">
+                Dashboard
+              </a>
+              <a href="#" className="axu bim blx aeb arl asb awg awk">
+                Team
+              </a>
+              <a href="#" className="axu bim blx aeb arl asb awg awk">
+                Projects
+              </a>
+              <a href="#" className="axu bim blx aeb arl asb awg awk">
+                Calendar
+              </a>
+            </div>
+          </div>
+          <div className="lx zg">
+            <div className="uw">
+              <button
+                type="button"
+                className="ab ly zg aaf aeb aju arl asb awg awm ban bbt bit bpb bpc bpe bpk"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                  className="hm of si"
+                >
+                  <path d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"></path>
+                </svg>
+                New Job
+              </button>
+            </div>
+            <div className="md cmb cmr cnr cok">
+              <button
+                type="button"
+                className="ab adz ajb aqs axv blx bnc bnh boe boj boo"
+              >
+                <span className="aa ag"></span>
+                <span className="t">View notifications</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                  className="oi sl"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                  ></path>
+                </svg>
+              </button>
+              <div className="ab jw" data-headlessui-state="">
+                <div>
+                  <button
+                    className="ab lx adz ajb awg bnc bnh boe boj boo"
+                    id="headlessui-menu-button-:r3:"
+                    type="button"
+                    aria-haspopup="menu"
+                    aria-expanded="false"
+                    data-headlessui-state=""
+                  >
+                    <span className="aa ag"></span>
+                    <span className="t">Open user menu</span>
+                    <img
+                      className="om sq adz"
+                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      alt=""
+                    />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default App;
