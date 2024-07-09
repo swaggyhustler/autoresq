d0d125b62f958ff214a872e526400a2e6b134066
swaggyhustler <gedamsubhash086@gmail.com>
2024-07-09 23:54:15 +0530

    Build frontend for application

---
+++ Frontend/src/componets/Navbar.jsx
@@ -0,0 +1,16 @@
+const Navbar=()=>{
+    return (
+        <nav className="flex justify-around p-5">
+            <span>AutoResQ</span>
+            <ul className="flex justify-around gap-5">
+                <li><a href="#">link1</a></li>
+                <li><a href="#">link2</a></li>
+                <li><a href="#">link3</a></li>
+                <li><a href="#">link4</a></li>
+                <li><a href="#">link5</a></li>
+            </ul>
+            <a href="8200342755">+91 8200342755</a>
+        </nav>
+    )
+}
+export default Navbar;
